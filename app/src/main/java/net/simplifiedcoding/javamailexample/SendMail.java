10fd5c4c2ff4ff78cbf3c1a0fba1317b740f804c
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2016-11-11 16:43:01 +0545

    Confirmation Link Sent to email, Random code inserted to the database

--- app/src/main/java/net/simplifiedcoding/javamailexample/SendMail.java
+++ app/src/main/java/net/simplifiedcoding/javamailexample/SendMail.java
@@ -47,7 +47,7 @@ public class SendMail extends AsyncTask<Void,Void,Void> {
     protected void onPreExecute() {
         super.onPreExecute();
         //Showing progress dialog while sending email
-        progressDialog = ProgressDialog.show(context,"Sending Confirmation Link","Please wait...",false,false);
+        progressDialog = ProgressDialog.show(context,"Sending Confirmation Link","Please wait for while ...",false,false);
     }
 
     @Override
