7f3e28de5e1c6a1cd9894202d9c938ae09614fad
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2017-01-18 16:46:54 +0545

    Update Config.java

--- app/src/main/java/net/simplifiedcoding/javamailexample/Config.java
+++ app/src/main/java/net/simplifiedcoding/javamailexample/Config.java
@@ -4,6 +4,6 @@ package net.simplifiedcoding.javamailexample;
  * Created by Belal on 10/30/2015.
  */
 public class Config {
-    public static final String EMAIL ="chaulagainajeet@gmail.com";
-    public static final String PASSWORD ="Itsajgmail14";
+    public static final String EMAIL ="your_email_address";
+    public static final String PASSWORD ="your_password";
 }
